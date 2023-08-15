2e46d001766577d25973f206abc4da9e2f7e875e
User <ayatabdelrhman514@gmail.com>
2023-08-15 19:47:30 +0300

    app init

---
+++ lib/homes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  static const String routeName="aaa";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      appBar: AppBar(
+        title: Text("islami"),
+      ),
+    ) ;
+  }
+}
